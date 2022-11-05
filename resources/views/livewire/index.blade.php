231ae498fd17d09f2cb486219a3e03098e28ff4d
thiagop127001 <thiagogua2009@gmail.com>
2022-11-05 07:49:35 -0300

    criado componente index

---
+++ resources/views/livewire/index.blade.php
@@ -0,0 +1,30 @@
+<div class="countainer">
+    <div class="primary">
+        <h1>Biblioteca de Estudos</h1>
+    </div>
+    <div class="cards">
+        <div class="transform">
+            <div class="card card1">
+                <h2>What is Lorem Ipsum?</h2>
+                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the
+                    industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type
+                    and scrambled it to make a type specimen book. </p>
+                <h4>Texto de Exemplo</h4>
+            </div>
+            <div class="card card2">
+                <h2>What is Lorem Ipsum?</h2>
+                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the
+                    industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type
+                    and scrambled it to make a type specimen book. </p>
+                <h4>Texto de Exemplo</h4>
+            </div>
+            <div class="card card3">
+                <h2>What is Lorem Ipsum?</h2>
+                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the
+                    industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type
+                    and scrambled it to make a type specimen book. </p>
+                <h4>Texto de Exemplo</h4>
+            </div>
+        </div>
+    </div>
+</div>
