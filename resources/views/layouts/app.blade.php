790fcd7acd384bdd153c41d0509b4c4adf3c0103
thiagop127001 <thiagogua2009@gmail.com>
2022-11-08 08:09:10 -0300

    add tailwind via cnd

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,13 +5,13 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <title>{{ config('app.name', 'Laravel') }}</title>
+        {{-- <title>{{ config('app.name', 'Laravel') }}</title> --}}
 
         <!-- Fonts -->
         <link rel="stylesheet" href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap">
-
+        <script src="https://cdn.tailwindcss.com"></script>
         <!-- Scripts -->
-        @vite(['resources/css/app.css','resources/css/genres.css', 'resources/js/app.js'])
+        {{-- @vite(['resources/css/app.css','resources/css/genres.css', 'resources/js/app.js']) --}}
 
         <!-- Styles -->
         
